293b65737ff268c7f5032c2774ce71903d11e1d5
himanshuvansal01 <vansal1332@gmail.com>
2022-08-03 05:55:26 +0530

    fix the parsing error

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+
+//middlewares
+app.use(express.json());
+
+app.get("/howdy",(req, res) => {
+
+    res.end("Hello Howdy!")
+})
+
+app.listen(8800, ()=>{
+    console.log("Connected to backend!");
+});
